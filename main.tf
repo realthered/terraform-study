c661c8f95c54c83f4a923d56bc7f4b0ffb8a0bf3
Seungjin Lee <latyiee@gmail.com>
2020-09-29 18:01:26 +0900

    Saving current status

--- main.tf
+++ main.tf
@@ -2,10 +2,36 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 resource "aws_instance" "example" {
-  ami              = "ami-0f96495a064477ffb"
+  ami              = "ami-0fff85c1d8aa5372b"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+                   #!/bin/bash
+                   echo "Hello, World!" > index.html
+                   nohup busybox httpd -f -p var.server_port &
+                   EOF
 
   tags = {
     Name = "terraform-example"
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port     = var.server_port
+    to_port         = var.server_port
+    protocol       = "tcp"
+    cidr_blocks    = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default       = 8080
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
 }
