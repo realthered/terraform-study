c22fd7c7a211a2b20da0566b65a24dd5303dad52
Seungjin Lee <latyiee@gmail.com>
2020-10-05 20:42:10 +0900

    Finishing modules

--- stage/data-stores/mysql/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "port" {
-  value = "${aws_db_instance.example.port}"
-}
-
-output "address" {
-  value = "${aws_db_instance.example.address}"
-}
--- stage/data-stores/mysql/vars.tf
+++
@@ -1,3 +0,0 @@
-variable "db_password" {
-  description = "The Password for the database"
-}
