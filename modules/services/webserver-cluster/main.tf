d2a0937f361f2cbc96f6ee47c62984ec9a75d8fc
realthered <latyiee@gmail.com>
2020-10-05 10:09:27 +0900

    Update main.tf

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -54,6 +54,17 @@ resource "aws_security_group" "instance" {
   }
 }
 
+
+resource "aws_security_group_rule" "allow_server_http_inbound" {
+  type              = "ingress"
+  security_group_id = "${aws_security_group.instance.id}"
+
+  from_port   = "${var.server_port}"
+  to_port     = "${var.server_port}"
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
 resource "aws_security_group_rule" "allow_http_inbound" {
   type = "ingress"
   security_group_id = aws_security_group.elb.id
